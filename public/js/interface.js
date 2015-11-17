6f3da85f853626824218d624472114fd8bd9a1df
danwheeler <da-wheeler@hotmail.co.uk>
2015-11-17 17:15:37 +0000

    mood testing

--- public/js/interface.js
+++ public/js/interface.js
@@ -5,5 +5,17 @@ $(document).ready( function() {
   });
 
   $('a[href="' + this.location.pathname + '"]').parents('li,ul').addClass('active');
+
+  $('#change').click( function() {
+  	var current = $( "#circle" ).attr('class');
+  	var next;
+  	if (current === 'mood1') {
+  		next = 'mood2';
+  	}
+  	else {
+  		next = 'mood1';
+  	}
+		$( "#circle" ).removeClass( current ).addClass( next );
+  });
 });
 
