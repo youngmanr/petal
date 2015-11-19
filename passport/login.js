43607ae78c5c6ea79cc13ab3efa5a615ed9a0517
Robert Youngman <rob_youngman@yahoo.com>
2015-11-19 12:58:45 +0000

    mongoosified

--- passport/login.js
+++ passport/login.js
@@ -1,16 +1,15 @@
 var LocalStrategy   = require('passport-local').Strategy;
+var User = require('../models/user');
 var bCrypt = require('bcrypt-nodejs');
 
 module.exports = function(passport){
 
-  passport.use('login', new LocalStrategy({
+    passport.use('login', new LocalStrategy({
             passReqToCallback : true
         },
-        function(req, username, password, done) { 
-            var db = req.db;
-            var collection = db.get('usercollection');
+        function(req, username, password, done) {
             // check in mongo if a user with username exists or not
-            collection.findOne({ 'username' :  username }, 
+            User.findOne({ 'username' :  username },
                 function(err, user) {
                     // In case of any error, return using the done method
                     if (err)
@@ -18,9 +17,9 @@ module.exports = function(passport){
                     // Username does not exist, log the error and redirect back
                     if (!user){
                         console.log('User Not Found with username '+username);
-                        return done(null, false, req.flash('message', 'User Not found.'));                 
+                        return done(null, false, req.flash('message', 'User Not found.'));
                     }
-                    // User exists but wrong password, log the error 
+                    // User exists but wrong password, log the error
                     if (!isValidPassword(user, password)){
                         console.log('Invalid Password');
                         return done(null, false, req.flash('message', 'Invalid Password')); // redirect back to login page
@@ -38,5 +37,5 @@ module.exports = function(passport){
     var isValidPassword = function(user, password){
         return bCrypt.compareSync(password, user.password);
     }
-    
+
 }
--- passport/signup.js
+++ passport/signup.js
@@ -1,4 +1,5 @@
 var LocalStrategy   = require('passport-local').Strategy;
+var User = require('../models/user');
 var bCrypt = require('bcrypt-nodejs');
 
 module.exports = function(passport){
@@ -9,10 +10,8 @@ module.exports = function(passport){
         function(req, username, password, done) {
 
             findOrCreateUser = function(){
-                var db = req.db;
-                var collection = db.get('usercollection');
                 // find a user in Mongo with provided username
-                collection.findOne({ 'username' :  username }, function(err, user) {
+                User.findOne({ 'username' :  username }, function(err, user) {
                     // In case of any error, return using the done method
                     if (err){
                         console.log('Error in SignUp: '+err);
@@ -24,25 +23,22 @@ module.exports = function(passport){
                         return done(null, false, req.flash('message','User Already Exists'));
                     } else {
                         // if there is no user with that email
-                       // // create the user
-                       //  var newUser = new User();
+                        // create the user
+                        var newUser = new User();
 
-                       //  // set the user's local credentials
-                       //  newUser.username = username;
-                       //  newUser.password = createHash(password);
-                       //  newUser.email = req.param('email');
-                       //  newUser.firstName = req.param('firstName');
-                       //  newUser.lastName = req.param('lastName');
+                        // set the user's local credentials
+                        newUser.username = username;
+                        newUser.password = createHash(password);
 
-                       //  // save the user
-                       //  newUser.save(function(err) {
-                       //      if (err){
-                       //          console.log('Error in Saving user: '+err);  
-                       //          throw err;  
-                       //      }
-                       //      console.log('User Registration succesful');    
-                       //      return done(null, newUser);
-                       //  });
+                        // save the user
+                        newUser.save(function(err) {
+                            if (err){
+                                console.log('Error in Saving user: '+err);
+                                throw err;
+                            }
+                            console.log('User Registration succesful');
+                            return done(null, newUser);
+                        });
                     }
                 });
             };
@@ -56,4 +52,5 @@ module.exports = function(passport){
     var createHash = function(password){
         return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
     }
+
 }
