0d5d0722b716fca2b0e620854a2fd827b2b82a4a
danwheeler <da-wheeler@hotmail.co.uk>
2015-11-19 15:56:59 +0000

    chart added

--- public/js/interface.js
+++ public/js/interface.js
@@ -2,6 +2,9 @@ $(document).ready( function() {
 
   $('a[href="' + this.location.pathname + '"]').parents('li,ul').addClass('active');
 
+
+  $('#arrow').css('transform', 'rotate(36deg)');
+ 
 };
 
 
