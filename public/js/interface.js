c51a296e0f323bd83ec5c04dfa188d6feebfd6cd
Andrew Dowell <adowellphoto@gmail.com>
2015-11-19 15:08:33 +0000

    added basic rotating arm

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,21 +1,8 @@
 $(document).ready( function() {
 
-  $('#submit').click( function() {
-    alert('you pressed submit?');
-  });
-
   $('a[href="' + this.location.pathname + '"]').parents('li,ul').addClass('active');
 
-  $('#change').click( function() {
-  	var current = $( "#circle" ).attr('class');
-  	var next;
-  	if (current === 'mood1') {
-  		next = 'mood2';
-  	}
-  	else {
-  		next = 'mood1';
-  	}
-		$( "#circle" ).removeClass( current ).addClass( next );
-  });
-});
+};
+
+
 
